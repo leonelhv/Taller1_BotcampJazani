ab755b7280d882503697528b62def67f61f56bf3
leonelhv <leonel.huachacav@gmail.com>
2023-10-02 20:39:31 -0500

    Add dbContext

---
+++ Jazani.Infrastructure/Cores/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Jazani.Infrastructure.Cores.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ Jazani.Infrastructure/Cores/Contexts/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jazani.Infrastructure.Cores.Contexts
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DbConnection"))
+            );
+
+            return services;
+        }
+    }
+}
